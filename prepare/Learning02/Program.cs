126876a4f644a615fb3461d1ed8afd379ba368e1
Francesco Lezano <srchesco@icloud.com>
2023-01-20 16:21:09 -0700

    lasthmw

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2005;
+        job1._endYear = 2007;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2009;
+        job2._endYear = 2013;
+
+        Resume myResume = new Resume();
+        myResume._name = "Francesco Lezano";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+    // testing 6
 }
