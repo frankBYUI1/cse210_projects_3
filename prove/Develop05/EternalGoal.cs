59ac215deefbec03fb147dc3e65b6cc122faea6b
Francesco Lezano <lezano.francesco@gmail.comm>
2023-03-28 10:48:39 -0600

    w10_developer

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class EternalGoal:Goal
+{
+    public EternalGoal(){
+
+    }
+    public EternalGoal(string type, string name, string description, string points):base(type, name, description, points){
+        
+   }
+}
--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,34 +1,78 @@
 using System;
 
 public class Goal
-{
+{   private int _type;
     private string _name;
     private string _description;
     private int _points;
 
-    public Goal(string name, string descrptn, int points) {
+    public Goal() {
+    }
+
+    public Goal(string type, string name, string description, string points){
+        _type = int.Parse(type);
         _name = name;
-        _description = descrptn;
-        _points = points;
+        _description = description;
+        _points = int.Parse(points);
     }
 
-    public virtual string getName () {
-        return _name;
+    public virtual int GetGoalType(){
+        return _type;
+    }
+    public virtual void SetGoalType(int typeOfGoal){
+        _type = typeOfGoal;
     }
 
-    public virtual string getDescription() {
-        return _description;
+
+    public virtual void SetGoal(){
+
+        Console.WriteLine("What is the name of your goal?");
+        _name = Console.ReadLine();
+        Console.WriteLine("what is a short description of it?");
+        _description = Console.ReadLine();
+        Console.WriteLine("What is the amount of points associated with this goal?");
+        _points = int.Parse(Console.ReadLine());
+
     }
 
-    public virtual int getPoints() {
+    public virtual void DisplayGoal() {
+        Console.WriteLine($"[ ] {_name} ({_description})");
+    }
+
+    public virtual int CheckGoal() {
         return _points;
     }
 
-    public virtual void displayGoal() {
-        Console.WriteLine($"{_name} ({_description})");
+    public virtual string GetGoalData(){
+        string goalData = $"{_type}, {_name}, {_description}, {_points}";
+        return goalData;
     }
 
+    public virtual void StringToGoal(string type, string name, string description, string points){
+        _type = int.Parse(type);
+        _name = name;
+        _description = description;
+        _points = int.Parse(points);
+    }
 
+    public virtual string GetName () {
+        return _name;
+    }
+    public virtual void SetName(string name){
+        _name = name;
+    }
 
+    public virtual string GetDescription() {
+        return _description;
+    }
+    public virtual void SetDescription(string description){
+        _description = description;
+    }
 
+    public virtual int GetPoints() {
+        return _points;
+    }
+    public virtual void SetPoints(int points){
+        _points = points;
+    }
 }
